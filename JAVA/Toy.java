431910c6754196a8b7bb3f0dc0edc3c60273f34b
Даниял Ахунов <daniyal.ahunov@icloud.com>
2023-09-09 14:44:14 +0300

    что то сделал уже запутался

--- JAVA/Toy.java
+++ JAVA/Toy.java
@@ -1,17 +1,18 @@
+package JAVA;
+
 public class Toy {
     private int id;
     private String name;
     private int quantity;
-    private double weight;
+    private double probability;
 
-    public Toy(int id, String name, int quantity, double weight) {
+    public Toy(int id, String name, int quantity, double probability) {
         this.id = id;
         this.name = name;
         this.quantity = quantity;
-        this.weight = weight;
+        this.probability = probability;
     }
-
-    public int getId() {
+        public int getId() {
         return id;
     }
 
@@ -19,28 +20,42 @@ public class Toy {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getQuantity() {
         return quantity;
     }
 
-    public double getWeight() {
-        return weight;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
-    public void updateWeight(double weight) {
-        this.weight = weight;
+    public double getProbability() {
+        return probability;
     }
+
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
+    public void updateProbability(double probability) {
+        this.probability = probability;
+    }
+    // Геттеры и сеттеры для свойств игрушек
 
     public boolean hasStock() {
         return quantity > 0;
     }
 
     public void decreaseQuantity() {
-        quantity--;
+        if (quantity > 0) {
+            quantity--;
+        }
     }
 
     @Override
     public String toString() {
-        return "ID: " + id + ", Название: " + name + ", Количество: " + quantity + ", Вес: " + weight + "%";
+        return "ID: " + id + ", Название: " + name + ", Количество: " + quantity + ", Шанс выпадения: " + probability + "%";
     }
 }
